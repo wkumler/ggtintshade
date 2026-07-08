e9c594c4ad7c52805c4f496abe416e2e60394142
wkumler <wkumler@uw.edu>
2026-07-08 14:45:36 -0700

    Adding proper main package docs and imports

--- R/ggtintshade-package.R
+++ R/ggtintshade-package.R
@@ -1,4 +1,16 @@
+#' @md
+#' @details
+#' The `ggtintshade` package is designed to provide an additional aesthetic to
+#' typical `ggplot2` plots that allows colors to be lightened/darkened by
+#' mapping the saturation/value coordinates as well as the hue. This allows for
+#' grouped, nested, and crossed designs where not all colors should be equally
+#' distinct.
+#'
+#' @keywords internal
+"_PACKAGE"
 
-# Import area
-#' @import ggplot2 grid rlang scales colorspace
+# All R code references ggplot2 with `ggplot2::` so that top-level object
+# creation works before this import is registered; the import makes ggplot2
+# available to the test namespace and documents the dependency.
+#' @import ggplot2
 NULL
