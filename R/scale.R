068363dfb9dc07f8003c513c57784887bb561ab5
wkumler <wkumler@uw.edu>
2026-07-14 13:49:17 -0700

    Patching scale to make sure other params are passed along

--- R/scale.R
+++ R/scale.R
@@ -7,6 +7,8 @@
 #' @param range Numeric length-2 output range within `[0, 1]`. Defaults to
 #'   `c(0.2, 0.8)` (dark to pale, base hue in the middle).
 #' @param aesthetics Aesthetic this scale applies to. Defaults to `"tintshade"`.
+#' @param guide The legend guide. Defaults to [guide_tintshade()]; pass a
+#'   configured `guide_tintshade(...)` to customise the legend.
 #' @param ... Passed to [ggplot2::discrete_scale()] or
 #'   [ggplot2::continuous_scale()].
 #'
@@ -24,21 +26,23 @@ NULL
 #' @rdname scale_tintshade
 #' @export
 scale_tintshade_discrete <- function(..., range = c(0.2, 0.8),
-                                      aesthetics = "tintshade") {
+                                     aesthetics = "tintshade",
+                                     guide = guide_tintshade()) {
   ggplot2::discrete_scale(
     aesthetics = aesthetics,
     palette = function(n) seq(range[1], range[2], length.out = n),
-    guide = guide_tintshade(), ...
+    guide = guide, ...
   )
 }
 
 #' @rdname scale_tintshade
 #' @export
 scale_tintshade_continuous <- function(..., range = c(0.2, 0.8),
-                                        aesthetics = "tintshade") {
+                                       aesthetics = "tintshade",
+                                       guide = guide_tintshade()) {
   ggplot2::continuous_scale(
     aesthetics = aesthetics,
     palette = function(x) scales::rescale(x, to = range),
-    guide = guide_tintshade(), ...
+    guide = guide, ...
   )
 }
